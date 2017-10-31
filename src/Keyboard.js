ba533e00ece057249dcf8d9ea23f20a9f30788ae
xtrinch <mojca.rojko@gmail.com>
2017-11-01 09:43:01 +1100

    Add back the accidentaly removed number

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -194,7 +194,7 @@ export default class Keyboard extends PureComponent {
 	render() {
 		const {rightButtons, inputNode, secondaryKeyboard} = this.props;
 		const keys = this.getKeys();
-		const numbers = [1, 2, 3, 4, 5, 7, 8, 9, 0];
+		const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 		const symbolsKeyValue = this.getSymbolsKeyValue();
 
 		return (
