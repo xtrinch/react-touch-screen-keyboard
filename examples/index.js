308d2a67a70d6219fa30b5404b1242c56963bc73
Regan Lawton <reganlawton92@gmail.com>
2017-11-01 10:55:21 +1100

    Add `webpack-dev-server` as a node dep and tweak start script

--- examples/index.js
+++ examples/index.js
@@ -3,43 +3,37 @@ import ReactDom from 'react-dom';
 import KeyboardedInput from '../src';
 import '../src/Keyboard.css';
 
-
-
 export default class MainComponent extends React.Component {
 
-    constructor(props)
-    {
-        super(props);
-        this.state = {value: '', value1: ''};
-        this.handleValueChange = this.handleValueChange.bind(this);
-        this.handleValue1Change = this.handleValue1Change.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = { value: '', value1: '' };
+    this.handleValueChange = this.handleValueChange.bind(this);
+    this.handleValue1Change = this.handleValue1Change.bind(this);
+  }
 
-    handleValueChange(val)
-    {
-        this.setState({value: val});
-    }
+  handleValueChange(val) {
+    this.setState({ value: val });
+  }
 
-    handleValue1Change(val)
-    {
-        this.setState({value1: val});
-        console.log(this.state.value1)
-    }
+  handleValue1Change(val) {
+    this.setState({ value1: val });
+    console.log(this.state.value1);
+  }
 
-    render()
-    {
-        return (
-			<div>
-			    <p>react-touch-screen-keyboard</p>
-			    <KeyboardedInput onChange={(value) => {this.handleValueChange(value)}} enabled />
-			    <br />
-			    <KeyboardedInput onChange={(value) => {this.handleValue1Change(value)}} enabled />
-			</div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <p>react-touch-screen-keyboard</p>
+        <KeyboardedInput onChange={(value) => { this.handleValueChange(value); }} enabled />
+        <br />
+        <KeyboardedInput onChange={(value) => { this.handleValue1Change(value); }} enabled />
+      </div>
+    );
+  }
 }
 
 ReactDom.render(
   <MainComponent />,
-  document.getElementById('app')
-)
+  document.getElementById('app'),
+);
