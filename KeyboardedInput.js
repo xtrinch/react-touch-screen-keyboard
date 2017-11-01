e34413de6d96ce94d64682a93a725f73f69774de
Mojca Rojko <mojca.rojko@gmail.com>
2017-11-01 11:09:43 +1100

    Delete KeyboardedInput.js

--- KeyboardedInput.js
+++
@@ -1,7 +0,0 @@
-var Keyboard = require('./lib/Keyboard');
-var KeyboardButton = require('./lib/KeyboardButton');
-var KeyboardedInput = require('./lib/KeyboardedInput');
-
-module.exports.Keyboard = Keyboard.default || Keyboard;
-module.exports = KeyboardedInput.default || KeyboardedInput;
-module.exports.KeyboardButton = KeyboardButton.default || KeyboardButton;
